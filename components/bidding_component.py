4d1020a90522b67f6e3d912a5a17a43dc5767a14
Lukeus Maximus <lm9131@bristol.ac.uk>
2012-11-28 13:51:58 +0000

    Converted bidding component

--- components/bidding_component.py
+++ components/bidding_component.py
@@ -1,5 +1,5 @@
 class BiddingComponent:
-    def __init__(self, side, limit_price, Instrument instrument):
+    def __init__(self, side, limit_price, instrument):
 
         self.limit_price = self.limit_price          
         self.first_trading_round = True
@@ -13,19 +13,14 @@ class BiddingComponent:
         self.best_bid = 0
         self.best_ask = 0
 
-        if (side == "bid")
-        {
-            InnerPrice = InnerPriceBuyer;
-        }
-        else
-        {
-            InnerPrice = InnerPriceSeller;
-        }
+        self.inner_price = self.inner_price_seller
+        if (side == "bid"):
+            self.inner_price = self.inner_price_buyer
 
 
     #public bool IsFirstTradingRound { get { return _isFirstTradingRound; } set { _isFirstTradingRound = value; } }
 
-    def update_best_prices(best_bid, best_ask):
+    def update_best_prices(self, best_bid, best_ask):
         if (best_bid == 0.0):
             best_bid = self.current_instrument.MinPrice
         if (best_ask == 0.0):
@@ -34,94 +29,55 @@ class BiddingComponent:
         self.best_bid = best_bid
         self.best_ask = best_ask
 
-    public double Price(double tau, out bool success)
-    {
-        if (!_isFirstTradingRound && double.IsNaN(tau))
-        {
-            success = false;
-            return double.NaN;                
-        }
-
-        double price = InnerPrice(tau, out success);
-        int ticksInPrice = (int)(price / _currentInstrument.PriceTick);
-        return (double)ticksInPrice * _currentInstrument.PriceTick;            
-    }
+    def price(self, tau)
+        if not self.first_trading_round and tau < 0:
+            return tau, False      
+        price, success = self.inner_price(tau);
+        ticksInPrice = int(price / self.current_instrument.PriceTick);
+        return float(ticksInPrice * self.current_instrument.PriceTick), success          
     
-    private double InnerPriceBuyer(double tau, out bool success)
-    {
-        double price = 0.0;
-        success = false;
+    def inner_price_buyer(self, tau)
+        price = 0.0
+        success = False
         
-        if (_limitPrice <= _bestBid)
-        {
-            success = true;
-            price = _limitPrice;
-        }
-        else if (_limitPrice >= _bestAsk && ((_bestAsk - _bestBid) <= MaxSpread))
-        {
-            success = true;
-            price = _bestAsk;
-        }
-        else if (_limitPrice > _bestBid)
-        {
-            success = true;
-            if (_isFirstTradingRound)
-            {
-                double bestAskPlus = (1.0 + LambdaRelative) * _bestAsk + LambdaAbsolute;
-                price = _bestBid + (Math.Min(_limitPrice, bestAskPlus) - _bestBid) / Eta;
-            }
-            else
-            {
-                if (_bestAsk <= tau)
-                {
-                    price = _bestAsk;
-                }
-                else
-                {
-                    price = _bestBid + (tau - _bestBid) / Eta;
-                }
-            }
-        }           
+        if self.limit_price <= self.best_bid):
+            success = True
+            price = self.limit_price
+        elif self.limit_price >= self.best_ask and self.best_ask - self.best_bid <= self.max_spread:
+            success = True
+            price = self.best_ask
+        elif self.limit_price > self.best_bid:
+            success = True
+            if self.first_trading_round:
+                best_ask_plus = (1.0 + self.LAMBDA_R) * self.bestAsk + self.LAMBDA_A;
+                price = self.best_bid + (min(self.limit_price, best_ask_plus) - self.best_bid) / self.eta;
+            else:
+                if self.best_ask <= tau:
+                    price = self.best_ask
+                else:
+                    price = self.best_bid + (tau - self.best_bid) / self.eta;
+        return price, success
 
-        return price;
-    }
+    def inner_price_seller(self, tau):
+        price = 0.0
+        success = False
 
-    private double InnerPriceSeller(double tau, out bool success)
-    {
-        double price = 0.0;
-        success = false;
+        if self.limit_price >= self.best_ask:
+            success = True
+            price = self.limit_price
+        elif self.limit_price <= self.best_bid and self.best_ask - self.best_bid <= self.max_spread:
+            success = True
+            price = self.best_bid
+        elif self.limit_price < self.best_ask:
+            success = True
+            if self.first_trading_round:
+                best_bid_minus = (1.0 - self.LAMBDA_R) * self.best_bid - self.LAMBDA_A
+                price = self.best_ask - (self.best_ask - max(self.limit_price, best_bid_minus)) / self.eta
+                self.first_trading_round = False
+            else:
+                if self.best_bid >= tau:
+                    price = self.best_bid
+                else:
+                    price = self.best_bsk - (self.best_ask - tau) / self.eta
+        return price, success
 
-        if (_limitPrice >= _bestAsk)
-        {
-            success = true;
-            price = _limitPrice;
-        }
-        else if (_limitPrice <= _bestBid && ((_bestAsk - _bestBid) <= MaxSpread))
-        {
-            success = true;
-            price = _bestBid;
-        }
-        else if (_limitPrice < _bestAsk)
-        {
-            success = true;
-            if (_isFirstTradingRound)
-            {
-                double bestBidMinus = (1.0 - LambdaRelative) * _bestBid - LambdaAbsolute;
-                price = _bestAsk - (_bestAsk - Math.Max(_limitPrice, bestBidMinus)) / Eta;
-                _isFirstTradingRound = false;
-            }
-            else
-            {
-                if (_bestBid >= tau)
-                {
-                    price = _bestBid;
-                }
-                else
-                {
-                    price = _bestAsk - (_bestAsk - tau) / Eta;
-                }
-            }
-        }           
-
-        return price;
-    }
